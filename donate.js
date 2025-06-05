b6ce29180c0272d3663c4aa04af8385eca84b652
rich-richards <richdevilrichdevil@gmail.com>
2025-06-05 17:40:26 +0100

    Add files via upload

---
+++ donate.js
@@ -0,0 +1,7 @@
+ const button = document.getElementById("buttons");
+    
+    button.innerHTML += `<div>
+    <button class="btn"><a class="link" href="https://www.paypal.me/ThierryMekulu" target=_blank>$0.49</a></button>
+    <button class="btn"><a class="link" href="https://www.paypal.me/ThierryMekulu" target=_blank>$0.79</a></button>
+    <button class="btn"><a class="link" href="https://www.paypal.me/ThierryMekulu" target=_blank>$0.99</a></button>
+    </div>`;
