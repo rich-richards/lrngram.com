787b2b69ded115fa94d3c6506a0b1555417aaf2f
rich-richards <richdevilrichdevil@gmail.com>
2025-11-08 16:54:16 +0100

    Delete JS-Script directory

--- JS-Script/donate.js
+++
@@ -1,7 +0,0 @@
- const button = document.getElementById("buttons");
-    
-    button.innerHTML += `<div>
-    <button class="btn"><a class="link" href="https://www.paypal.me/ThierryMekulu">$0.69</a></button>
-    <button class="btn"><a class="link" href="https://www.paypal.me/ThierryMekulu">$0.79</a></button>
-    <button class="btn"><a class="link" href="https://www.paypal.me/ThierryMekulu">$0.99</a></button>
-    </div>`;
